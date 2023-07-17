968d2c85fe8b27b97ce1c476d09b3fb3d6fcbfe9
Rahil Ansari <12849916+rahilansari261@users.noreply.github.com>
2023-07-17 05:19:33 +0000

    api key env added

--- src/pages/MovieDetail.js
+++ src/pages/MovieDetail.js
@@ -17,7 +17,7 @@ export const MovieDetail = () => {
   useEffect(() => {
     async function fetchMovie() {
       const response = await fetch(
-        `https://api.themoviedb.org/3/movie/${params.id}?api_key=2f9682de901722ed1f9c808a4a29e197`
+        `https://api.themoviedb.org/3/movie/${params.id}?api_key=${process.env.REACT_APP_API_KEY}`
       )
       const json = await response.json()
       setMovie(json)
